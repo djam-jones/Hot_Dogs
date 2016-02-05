5bde843d06b787f9929b21f3698d24850374aca8
Collix <15986@ma-web.nl>
2016-02-05 13:18:08 +0100

    Imported spritesheets and wrote controller.

---
+++ Hot_Dogs/Assets/Scripts/Game/Animations/Salesman/SalesmanController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SalesmanController : MonoBehaviour
+{
+    private Animator anims;
+
+    // Timer variables.
+    private float currentTime;
+    private float targetTime;
+    // Use this for initialization
+    void Start()
+    {
+        anims = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > targetTime)
+        {
+            anims.SetTrigger("Eat");
+            targetTime = Random.Range(10, 30);
+            currentTime = 0;
+            anims.SetTrigger("Idle");
+        }
+    }
+}

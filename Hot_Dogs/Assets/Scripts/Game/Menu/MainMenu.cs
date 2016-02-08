5523f1716399dd3963ebd166b5fabe6207e6d2db
Djamali Shawn Jones <15546@ma-web.nl>
2016-02-08 16:10:56 +0100

    Just Menu Things
    Animations and such

--- Hot_Dogs/Assets/Scripts/Game/Menu/MainMenu.cs
+++ Hot_Dogs/Assets/Scripts/Game/Menu/MainMenu.cs
@@ -4,15 +4,34 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	void OnLevelWasLoaded()
+	private MenuAnimations _menuAnimsScript;
+	private Animator _menuAnim;
+
+	void Awake()
 	{
-		//Do Menu Animations.
+		_menuAnimsScript = GameObject.Find("Menu Handler").GetComponent<MenuAnimations>();
+		_menuAnim = _menuAnimsScript.GetComponent<Animator>();
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		if(level == 0)
+		{
+			//Do Menu Animations.
+			_menuAnimsScript.SwiffUpAnimation(_menuAnim);
+		}
 	}
 
 	public void StartGame()
 	{
 		//Loading Screen.
 
+		Invoke("LoadGameScene", 4);
+		_menuAnimsScript.IntroAnimation(_menuAnim);
+	}
+
+	private void LoadGameScene()
+	{
 		SceneManager.LoadScene("Physics_Djamali"); //Load Main Scene.
 	}
 
--- Hot_Dogs/Assets/Scripts/Game/Menu/MenuAnimations.cs
+++ Hot_Dogs/Assets/Scripts/Game/Menu/MenuAnimations.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class MenuAnimations : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public void IntroAnimation(Animator anim)
+	{
+		anim.SetTrigger("Intro");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void SwiffUpAnimation(Animator anim)
+	{
+		anim.SetTrigger("Swiff");
 	}
 }

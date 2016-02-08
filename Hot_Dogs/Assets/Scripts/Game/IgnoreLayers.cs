ce315378954662501528661add81702459479cdb
Djamali Shawn Jones <15546@ma-web.nl>
2016-02-08 11:37:13 +0100

    Push
    Sprites, Animations, Scenes, Scripts -> CameraShake, IgnoreLayers,
    MainMenu, Animations, Movement, Tags

---
+++ Hot_Dogs/Assets/Scripts/Game/IgnoreLayers.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Layers {
+	None, 
+	Border, 
+	Player, 
+	UI
+}
+
+public class IgnoreLayers : MonoBehaviour {
+
+	public Layers layers;
+	private string _layerString = "";
+	private int _layerInt;
+
+	void Update()
+	{
+		LayerSwitch();
+		Ignore();
+	}
+
+	private void Ignore()
+	{
+		Physics2D.IgnoreLayerCollision(this.gameObject.layer, _layerInt);
+	}
+
+	public void LayerSwitch()
+	{
+		switch(layers)
+		{
+		case Layers.None:
+			_layerString = "";
+			break;
+
+		case Layers.Border:		
+			_layerString = Tags.BORDERTAG;
+			_layerInt = 8;
+			//_layerInt = 1 << LayerMask.NameToLayer(_layerString);
+			break;
+
+		case Layers.Player:
+			_layerString = Tags.PLAYERTAG;
+			_layerInt = 10;
+			//_layerInt = 1 << LayerMask.NameToLayer(_layerString);
+			break;
+
+		case Layers.UI:
+			_layerString = Tags.UITAG;
+			_layerInt = 5;
+			//_layerInt = 1 << LayerMask.NameToLayer(_layerString);
+			break;
+		}
+	}
+
+}
---
+++ Hot_Dogs/Assets/Scripts/Game/Menu/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnLevelWasLoaded()
+	{
+		//Do Menu Animations.
+	}
+
+	public void StartGame()
+	{
+		//Loading Screen.
+
+		SceneManager.LoadScene("Physics_Djamali"); //Load Main Scene.
+	}
+
+	public void QuitGame()
+	{
+		//Save Highscore.
+
+		Application.Quit(); //Quit Application.
+	}
+
+}

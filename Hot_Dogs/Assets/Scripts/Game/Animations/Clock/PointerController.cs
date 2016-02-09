2d7e34b5e57e2f5cc701835c14ddc9c8fdbfb255
Collix <15986@ma-web.nl>
2016-02-09 09:27:19 +0100

    Added clock

---
+++ Hot_Dogs/Assets/Scripts/Game/Animations/Clock/PointerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+
+    private Vector3 turnSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        turnSpeed = new Vector3(0, 0, 0.1f * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.transform.rotation.z);
+
+        if(this.transform.rotation.z >= 0)
+        {
+            this.transform.Rotate(turnSpeed);
+        }
+        else
+        {
+
+        }
+
+
+    }
+}

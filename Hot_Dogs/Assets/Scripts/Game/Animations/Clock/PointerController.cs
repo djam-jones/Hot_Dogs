4b70a57bcfb890a87401f924c6c6dcc6fc9e282a
Collix <15986@ma-web.nl>
2016-02-09 11:57:36 +0100

    Timer added

--- Hot_Dogs/Assets/Scripts/Game/Animations/Clock/PointerController.cs
+++ Hot_Dogs/Assets/Scripts/Game/Animations/Clock/PointerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class PointerController : MonoBehaviour
 {
@@ -7,11 +8,15 @@ public class PointerController : MonoBehaviour
     private float speed = 1;
 
     private Vector3 turnSpeed;
+    [SerializeField]
+    private UnityEvent finishedEvent;
+    private int winner;
 
     // Use this for initialization
     void Start()
     {
-        turnSpeed = new Vector3(0, 0, 0.1f * speed);
+        turnSpeed = new Vector3(0, 0, -0.1f * speed);
+        winner = 1;
     }
 
     // Update is called once per frame
@@ -19,13 +24,20 @@ public class PointerController : MonoBehaviour
     {
         Debug.Log(this.transform.rotation.z);
 
-        if(this.transform.rotation.z >= 0)
+        if(this.transform.rotation.z <= 0)
         {
             this.transform.Rotate(turnSpeed);
         }
         else
         {
+            if(winner == 1)
+            {
+                finishedEvent.Invoke();
+            }
+            else if(winner == 2)
+            {
 
+            }
         }
 
 
---
+++ Hot_Dogs/Assets/Scripts/Game/UI/UIslide.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIslide : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private float speed;
+
+    private bool sliding = false;
+
+    // Timer vars
+    private float currentTime;
+    [SerializeField]
+    private float targetTime;
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sliding == true)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
+        }   
+    }
+
+    public void startSlide()
+    {
+        sliding = true;
+    }
+}
